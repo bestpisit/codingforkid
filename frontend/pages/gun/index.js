71920de5f7243100db197a0d096d13d64b1c309f
FloatToast <39612@prc.ac.th>
2024-06-15 10:23:13 +0700

    ming update

--- frontend/pages/gun/index.js
+++ frontend/pages/gun/index.js
@@ -1,60 +1,150 @@
-document.getElementById('send-button').addEventListener('click',clickEvent);
+//Setup
+const chatConfiguration = {
+    name: "chats-best"
+};
 
-function clickEvent(){
-    var text = document.getElementById('inputbox').value;
-    addMessage(localStorage.getItem('username'),text);
-    chatMessages.push(
-        {
-            sender: localStorage.getItem('username'),
-            message: text
-        }
-    );
-    //add to localStorage
-    localStorage.setItem('chats-gun',JSON.stringify(chatMessages));
+const chatMessages = readMessages(chatConfiguration.name);
 
+main();
+function main() {
+    displayMessages(chatMessages);
 }
 
-function addMessage(user,message){
+//  Events
+document.getElementById('send-button').addEventListener('click', handleSendMessage);
+document.getElementById('back-button').addEventListener('click', () => window.history.back());
+
+//  Functions
+function handleSendMessage() {
+    const text = document.getElementById("inputbox").value;
+    if (!text) { return; }
+    document.getElementById("inputbox").value = '';
+    createMessage(localStorage.getItem('username'), text, chatMessages);
+    updateMessages(chatConfiguration.name, chatMessages);
+    displayMessages(chatMessages);
+}
+
+function displayMessages(messages) {
     const chat = document.getElementById('chat');
-    chat.innerHTML += `<div class="message"> <div class="sender"> ${user} </div> <div class="text"> ${message} </div> </div>`;
+    chat.innerHTML = '';
+    for (i of messages) {
+        const sender = i.sender;
+        const message = i.message;
+        chat.innerHTML += `<div class="message"> <div class="sender"> ${sender} </div> <div class="text"> ${message} </div> </div>;`
+    }
 }
 
-document.getElementById('back-button').addEventListener('click',backEvent);
-
-function backEvent(){
-    window.history.back();
+// CRUD
+function createMessage(sender, message, messages) {
+    messages.push(
+        {
+            sender: sender,
+            message: message
+        }
+    )
 }
 
-//input
-var chatMessages = JSON.parse(localStorage.getItem('chats-gun'));
-
-for(let i of chatMessages){
-    addMessage(i.sender,i.message);
+function readMessages(key) {
+    const messages = localStorage.getItem(key);
+    if (!messages) {
+        return [];
+    }
+    return JSON.parse(messages);
 }
 
-console.log(localStorage.getItem('chats-gun'));
-if(chatMessages == null){
-    chatMessages = [];
+function updateMessages(key, messages) {
+    localStorage.setItem(key, JSON.stringify(messages));
 }
 
+function deleteMessage() {
+    //TODO
+} 
 
-//process
 
-// for(let i = 0;i<5;i++){
-//     console.log(i)
+// //set up
+// //input
+// var chatMessages = readMessage();
+// console.log
+
+// for (let i of chatMessages) {
+//     createMessage(i.sender, i.message);
 // }
 
-// for(let i in user){
-//     console.log(user[i]+": ",message[i]);
-// }
-// for(let i of user){
-//     console.log(i);
+// //events
+// document.getElementById('send-button').addEventListener('click', clickEvent);
+// document.getElementById('back-button').addEventListener('click', backEvent);
+
+// //functions
+// function clickEvent() {
+//     var text = document.getElementById('inputbox').value;
+//     createMessage(localStorage.getItem('username'), text);
+//     chatMessages.push(
+//         {
+//             sender: localStorage.getItem('username'),
+//             message: text
+//         }
+//     );
+//     //add to localStorage
+//     localStorage.setItem('chats-gun', JSON.stringify(chatMessages));
+
 // }
 
-//output
-// console.log(sum(user,message));
-
-//function
-// function sum(user,message){
-//     return user+": "+message;
+// function backEvent() {
+//     window.history.back();
 // }
+
+
+// console.log(localStorage.getItem('chats-gun'));
+
+
+// function createMessage(user, message) {
+//     const chat = document.getElementById('chat');
+//     chat.innerHTML += `<div class="message"> <div class="sender"> ${user} </div> <div class="text"> ${message} </div> </div>`;
+//     chatMessages.push(
+//         {
+//             sender: localStorage.getItem('username'),
+//             message: message
+//         }
+//     );
+//     updateMessage();
+// }
+
+// function readMessage() {
+//     const chatData = localStorage.getItem('chats-gun');
+//     if (chatData == null) {
+//         return [];
+//     }
+//     else {
+//         return JSON.parse(chatData);
+//     }
+// }
+
+// function updateMessage(messages) {
+//     localStorage.setItem('chats-gun', JSON.stringify(messages));
+// }
+
+// function deleteMessage(messages) {
+
+// }
+
+
+// //process
+
+// // for(let i = 0;i<5;i++){
+// //     console.log(i)
+// // }
+
+// // for(let i in user){
+// //     console.log(user[i]+": ",message[i]);
+// // }
+// // for(let i of user){
+// //     console.log(i);
+// // }
+
+// //output
+// // console.log(sum(user,message));
+
+// //function
+// // function sum(user,message){
+// //     return user+": "+message;
+// // }

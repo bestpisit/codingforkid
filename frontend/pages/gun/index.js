040f358a660c187ee66a5f15f5d6fbd63ac6b446
FloatToast <39612@prc.ac.th>
2024-06-15 11:39:30 +0700

    debug+bug update

--- frontend/pages/gun/index.js
+++ frontend/pages/gun/index.js
@@ -31,7 +31,7 @@ function displayMessages(messages) {
     for (i in messages) {
         const sender = messages[i].sender;
         const message = messages[i].message;
-        chat.innerHTML += `<div class="message"><div class="sender"> ${sender} </div><div class="text"> ${message} </div><button onclick="deleteMessage(${i})">x</button></div>`;
+        chat.innerHTML += `<div class="message"><div class="sender"> ${sender} </div><div class="text"> ${message} </div><button class="del-button" onclick="deleteMessage(${i})">x</button></div>`;
     }
 }
 
